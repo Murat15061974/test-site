928e95995e9a6c0fd331e33eafe7a17696d1487a
Murat15061974 <ddurdymyrat85@gmail.com>
2025-03-09 23:14:34 +0500

    Create Index.js
    Bash
    
    git add .
    git commit -m "Добавлена главная
    страница"
    git push origin main

---
+++ Index.js
@@ -0,0 +1,12 @@
+export default function Home()
+{
+  return (
+    <div style={{ padding:
+    '20px' }}>
+<h1> Добро пожаловать на
+мой сайт!</h1>
+<p> Это мой первый проект на Next.js, хостится на
+Vercel.</p>
+  </div>
+);
+}
